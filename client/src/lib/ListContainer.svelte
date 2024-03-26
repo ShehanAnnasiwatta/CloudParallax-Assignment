f358b1ec6699ded30bb33cfc78b187ef3934cce5
Shehan <shehanjr99@gmail.com>
2024-03-27 05:22:00 +0530

    Restored Clinet and created a basic grid

---
+++ client/src/lib/ListContainer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let prodList: any[] = [];
+	const getProducts = async () => {
+		const res = await fetch('https://dummyjson.com/products');
+		const products = (await res.json()).products;
+		console.log(products);
+		prodList = products.map((prod: any) => {
+			return prod;
+		});
+	};
+
+	getProducts();
+</script>
+
+<div class="h-full w-full p-10">
+	<div class="h-full w-11/12  bg-slate-400 p-10 rounded-lg grid grid-cols-3">
+		{#each prodList as p}
+			<div class="h-220 w-350 p-5 bg-white m-4 md:h-72 w-96">
+				<h6>{p.title}</h6>
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import ListContainer from "$lib/ListContainer.svelte";
+
+</script>
+<!-- <h1 class="text-red-200">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<ListContainer />
